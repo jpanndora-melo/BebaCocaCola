14556541c5940486a1b4a25b03a400d74318da51
Jessica <jpanndora@gmail.com>
2025-11-01 18:09:50 -0300

    Atividade Concluida

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write(@"
+ beba  coca  cola
+     babe        cola
+     beba  coca
+     babe  cola  caco
+     caco
+     cola
+          c l o a c a
+");
+Console.ResetColor();
